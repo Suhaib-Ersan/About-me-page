de85982d049db5a69e96ae519b05543ad5de6bb8
Suhaib Ersan <suhaib.m.ersan@gmail.com>
2021-04-27 15:43:32 +0300

    x

--- js/app.js
+++ js/app.js
@@ -1,42 +1,70 @@
-// let mansafQuestion = prompt('Do you like mansaf?').toLowerCase;
+alert(hello)
+{
+  let greeting;
+  let today = new Date();
+  yes;
+  let hourNow = today.getHours();
+  yes;
 
-function askMansafQuestion (answer) {switch (mansafQuestion) {
-case 'yes':
-case 'y': {
-  let LikeMansafOrNot = 'what a coincidence, I also like mansaf!';
-  let mansafQuestionYes = document.getElementById('mansafPreference');
-  mansafQuestionYes.innerHTML = LikeMansafOrNot;
+  if (hourNow > 18) {
+    greeting = "Good evening";
+  } else if (hourNow > 12) {
+    greeting = "Good afternoon";
+  } else if (hourNow > 0) {
+    greeting = "Good morning";
+  } else {
+    greeting = "Welcome";
+  }
+
+  let choosenGreeting = document.getElementById("greetingText");
+  choosenGreeting.innerHTML = greeting;
 }
-  break;
-case 'no':
-case 'n': {
-  let LikeMansafOrNot = 'you don\'t like mansaf? you\'re one of a kind!';
-  let mansafQuestionYes = document.getElementById('mansafPreference');
-  mansafQuestionYes.innerHTML = LikeMansafOrNot;
-}
-}
+
+{
+  let userName = prompt("What's your name?");
+  let userNameToHtml = document.getElementById("userNameText");
+  userNameToHtml.innerHTML = userName;
 }
 
-askMansafQuestion(mansafQuestion);
+{
+  let knowYarmoukQ = prompt("Do you know a university by the name of Yarmouk?");
 
+  switch (knowYarmoukQ.toLowerCase) {
+    case "yes":
+    case "y":
+      {
+        let yesText =
+          "it's a well known university, so it's not weird you've heard of it";
+        let yesTextToHtml = document.getElementById("knowYarmoukText");
+        yesTextToHtml.innerHTML = yesText;
+      }
+      break;
+    case "no":
+    case "n": {
+      let noText = "too bad you've never heard of it";
+      let noTextToHtml = document.getElementById("knowYarmoukText");
+      noTextToHtml.innerHTML = noTextToHtml;
+    }
+  }
+}
 
+{
+  let likeMansafQ = prompt("Do you like mansaf?");
 
-
-// {
-//     var greeting;
-//     var today = new Date();yes
-//     var hourNow = today.getHours();yes
-
-//     if (hourNow > 18) {
-//         greeting = "Good evening";
-//     } else if (hourNow > 12) {
-//         greeting = "Good afternoon";
-//     } else if (hourNow > 0) {
-//         greeting = "Good morning";
-//     } else {
-//         greeting = "Welcome";
-//     }
-
-//     var greeting_space2 = document.getElementById("greetingSpace");
-//     greeting_space2.innerHTML = greeting;
-// }
+  switch (likeMansafQ.toLowerCase) {
+    case "yes":
+    case "y":
+      {
+        let yesText = "what a coincidence, I also like mansaf!";
+        let yesTextToHtml = document.getElementById("mansafPreferenceText");
+        yesTextToHtml.innerHTML = yesText;
+      }
+      break;
+    case "no":
+    case "n": {
+      let noText = "you don't like mansaf? you're one of a kind!";
+      let noTextToHtml = document.getElementById("mansafPreferenceText");
+      noTextToHtml.innerHTML = noTextToHtml;
+    }
+  }
+}
